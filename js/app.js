62391b4fca0ca93c08bdc8c75355044b0a16abdd
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-08-04 14:17:10 -0700

    Almost done with tic tac toe just need scoreboard and the check for tie function

--- js/app.js
+++ js/app.js
@@ -1,50 +1,71 @@
-console.log('Hello world');
+ole.log('Hello world');
 
+// Starts the game on click
 function startGame() {
+
+	for (var i = 1; i <= 9; i = i + 1) {
+		clearCell(i);
+	}
+
+	// decides turn on a .5 chance to be x or o
 	document.turn = "X";
+	if (Math.random() < 0.5) {
+		document.turn = "O";
+	}
+	document.winner = null;
 	setMessage(document.turn + " gets to start.");
 }
 
+// Function to change the msg in the html document 
 function setMessage(msg) {
 	document.getElementById("message").innerText = msg;
 }
 
+// Move turn function
 function nextMove(cell) {
-	if (cell.innerText == ""){
+	if(document.winner != null) {
+		setMessage(document.winner + " already won the game.");
+	} else if (cell.innerText == "") {
 		cell.innerText = document.turn;
 		switchTurn();
 	} else {
-		setMessage("That square is full.")
+		setMessage("That cell is full.")
 	}
 }
 
+// Switch turn function
 function switchTurn() {
-	if (document.turn == "X") {
+	if (checkForWin(document.turn)) {
+		setMessage("Congrats, " + document.turn + "! You win!");
+		document.winner = document.turn;
+	} else if (document.turn == "X") {
 		document.turn = "O";
+		setMessage("It's " + document.turn + "'s turn!");
 	} else {
 		document.turn = "X";
+		setMessage("It's " + document.turn + "'s turn!");
 	}
-
-	setMessage("It's " + document.turn + "'s turn!");
 }
 
+// Checking cells in a row for a win after the last move
 function checkForWin(move) {
 	var result = false;
-	if (checkWin(1, 2, 3, move) || 
-		checkWin(4, 5, 6, move) || 
-		checkWin(7, 8, 9, move) ||
-		checkWin(1, 4, 7, move) ||
-		checkWin(2, 5, 8, move) ||
-		checkWin(3, 6, 9, move) ||
-		checkWin(1, 5, 9, move) ||
-		checkWin(3, 5, 7, move)) {
+	if (checkCell(1, 2, 3, move) || 
+		checkCell(4, 5, 6, move) || 
+		checkCell(7, 8, 9, move) ||
+		checkCell(1, 4, 7, move) ||
+		checkCell(2, 5, 8, move) ||
+		checkCell(3, 6, 9, move) ||
+		checkCell(1, 5, 9, move) ||
+		checkCell(3, 5, 7, move)) {
 
 		result = true;
 	}
 	return result;
 }
 
-function checkWin(a, b, c, move) {
+// Check win to be used in checkForWin function
+function checkCell(a, b, c, move) {
 	var result = false;
 	if (getCell(a) == move && getCell(b) == move && getCell(c) == move) {
 		result = true;
@@ -52,6 +73,11 @@ function checkWin(a, b, c, move) {
 	return result;
 }
 
+// Get cell 'c' and number
 function getCell(number) {
-	document.getElementById("c" + number).innerText;
+	return document.getElementById("c" + number).innerText;
+}
+
+function clearCell(number) {
+	document.getElementById("c" + number).innerText = "";
 }
